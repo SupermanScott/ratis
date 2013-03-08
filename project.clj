e2247ba49dfda1deb0d66983e2c54936445dc944
Scott Reynolds <scott@scottreynolds.us>
2013-03-07 18:54:06 -0800

    setup project file a bit

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject ratis "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Ratis is a Redis proxy"
+  :url "http://github.com/SupermanScott/ratis"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.0"]]
   :main ratis.core)
