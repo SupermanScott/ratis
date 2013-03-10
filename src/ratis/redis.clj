3ed7736a23af8e0b77264fe90de37d0504680369
Scott Reynolds <scott@scottreynolds.us>
2013-03-09 17:06:25 -0800

    be able to send commands to redis

--- src/ratis/redis.clj
+++ src/ratis/redis.clj
@@ -2,7 +2,9 @@
   (:require
    [aleph.redis.protocol :as proto]
    [clojure.string :as str]
-   [gloss.io]))
+   [gloss.io]
+   [aleph.tcp]
+   [lamina.core]))
 
 (def redis-codec (proto/redis-codec :utf-8))
 
@@ -76,3 +78,11 @@
   "Returns true / false if the command is for master only"
   [cmd]
   (contains? master-only (str/upper-case (first cmd))))
+
+(defn send-to-redis
+  "Sends the command to the specified host and returns the response"
+  [host port cmd]
+  (let [ch (lamina.core/wait-for-result
+            (aleph.tcp/tcp-client {:host host :port port :frame redis-codec}))]
+    (lamina.core/enqueue ch cmd)
+    (lamina.core/wait-for-message ch)))
