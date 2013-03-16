7bcefcd252e67a02a42d0d5dce24bcbbeba9f690
Scott Reynolds <scott@scottreynolds.us>
2013-03-16 16:35:23 -0700

    make the servers agents and return the pool

--- src/ratis/config.clj
+++ src/ratis/config.clj
@@ -16,13 +16,14 @@
 (defn start-handler
   "Starts up a handler for a pool by creating the pool and servers"
   [pool-map]
-  (let [servers (map #(Server. (:host %) (:port %) (get % :priority 1))
+  (let [servers (map #(agent (->Server (:host %) (:port %) (get % :priority 1)))
                      (get pool-map :servers []))
         server_retry_timeout (:server_retry_timeout pool-map)
         server_failure_limit (:server_failure_limit pool-map)
-        pool (Pool. server_retry_timeout server_failure_limit servers)
+        pool (->Pool server_retry_timeout server_failure_limit servers)
         config {:port (:port pool-map) :frame redis/redis-codec}]
-    (aleph.tcp/start-tcp-server routing/redis-handler config)))
+    (aleph.tcp/start-tcp-server routing/redis-handler config)
+    pool))
 
 (defn start-handlers
   "Starts the handlers defined in the configuration file in path"
